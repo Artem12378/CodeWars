309c59d7f2cb768df12489405287f5b132c81d02
Artem12378 <susidko.2012@mail.ru>
2026-05-07 09:32:55 +0300

    TasksCodeWars

--- main.js
+++ main.js
@@ -171,3 +171,126 @@ console.log(digital_root(493193))
 
 
 
+/* Строка считается написанной в регистре заголовка, если каждое слово в ней либо (а) написано с заглавной буквы (то есть, только первая буква слова написана заглавной), либо (б) считается исключением и полностью переводится в нижний регистр, за исключением первого слова, которое всегда пишется с заглавной буквы.
+
+Напишите функцию, которая преобразует строку в регистр символов, заданный необязательным списком исключений (несущественных слов). Список несущественных слов должен быть представлен в виде строки, где каждое слово разделено пробелом. Ваша функция должна игнорировать регистр строки с несущественными словами — она должна вести себя одинаково, даже если регистр строки с несущественными словами изменится.
+
+Аргументы (Хаскелл)
+Первый аргумент : список второстепенных слов, разделённых пробелами, которые всегда должны быть написаны строчными буквами, за исключением первого слова в строке.
+Второй аргумент : исходная строка, которую необходимо преобразовать.
+Аргументы (другие языки)
+Первый аргумент (обязательный) : исходная строка, которую необходимо преобразовать.
+Второй аргумент (необязательный) : список второстепенных слов, разделённых пробелами, которые всегда должны быть написаны строчными буквами, за исключением первого слова в строке. Тесты JavaScript/CoffeeScript пройдут успешно, undefinedесли этот аргумент не используется.
+Пример
+titleCase('a clash of KINGS', 'a an the of') // should return: 'A Clash of Kings'
+titleCase('THE WIND IN THE WILLOWS', 'The In') // should return: 'The Wind in the Willows'
+titleCase('the quick brown fox') // should return: 'The Quick Brown Fox' */
+
+function titleCase(title, minorWords) {
+    if (!title) return '';
+
+    
+    let UpdateTitle = title.split(' ')
+    let newMinorWords = minorWords ? minorWords.toLowerCase().split(' ') : []
+    let result = []
+    UpdateTitle.map((el, i) => {
+            let lowerEl = el.toLowerCase();
+            if(newMinorWords.includes(lowerEl) ){
+                return result.push(lowerEl)
+                
+            } else{
+                return result.push(el[0].toUpperCase() + el.slice(1).toLowerCase())
+           
+            }
+    })
+
+    let newRes = result[0][0].toUpperCase() + result[0].slice(1).toLowerCase();
+                //`${result[0].toUpperCase()} ${result.slice(1).join(' ')} `;  
+    
+    
+    if (result.length > 1) {
+        newRes += ' ' + result.slice(1).join(' ');
+    }
+    return newRes
+}
+console.log(titleCase('a clash of KINGS', 'a an the of'))
+
+/*  You have passed all of the tests! :)  */
+
+/* 6
+В этом задании вам нужно, получив строку, заменить каждую букву на её порядковый номер в алфавите.
+Если символ в тексте не является буквой, игнорируйте его и не включайте в результат.
+"a" = 1, "b" = 2 и так далее.
+
+Пример:
+Входная строка: "The sunset sets at twelve o' clock."
+Выходная строка: "20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 15 3 11"
+
+(То есть для каждой буквы (в любом регистре) выдаётся её номер, все номера разделяются пробелами, небуквенные символы пропускаются.)
+*/
+function alphabetPosition(text) {
+    const newText = text.toLowerCase()
+    const rusLower = 'абвгдеёжзийклмнопрстуфхцчшщъыьэюя';
+    const rusUpper = rusLower.toUpperCase();
+    const rus = rusLower + rusUpper;
+    // 
+
+    const engLower = `abcdefghijklmnopqrstuvwxyz`
+    const engUpper = engLower.toUpperCase();
+    const eng = engLower + engUpper;
+    
+    if(rus.includes(text)){
+        const rusWordNum = rusLower.indexOf(`${newText}`) + 1; 
+        return rusWordNum
+    }
+    if(eng.includes(text)){
+
+        const engWordNum = engLower.indexOf(`${newText}`) + 1; 
+        return engWordNum
+    }
+  
+}
+
+console.log(alphabetPosition(`BAFSASFASFASFASFASFF`))
+
+
+
+/* У книжного магазина много книг, разделенных на 26 категорий, обозначенных буквами A, B, C, ..., Z.
+ Каждая книга имеет код, состоящий как минимум из 3 символов.
+  Первый символ кода — заглавная буква, определяющая категорию книги.
+ */
+
+/* # the bookseller's stocklist:
+"ABART 20", "CDXEF 50", "BKWRK 25", "BTSQZ 89", "DRTYM 60"
+
+# list of categories: 
+"A", "B", "C", "W"
+
+# result:
+"(A : 20) - (B : 114) - (C : 50) - (W : 0)" */
+
+function stockList(books, categories) {
+    return '';
+}
+
+
+/* Подсчёт количества дубликатов
+Напишите функцию, которая возвращает количество различных (уникальных) символов (букв латинского алфавита без учёта регистра и цифр), встречающихся во входной строке более одного раза.
+Можно считать, что входная строка содержит только буквы (в верхнем и нижнем регистре) и цифры.
+
+Примеры
+javascript
+"abcde" → 0            # ни один символ не повторяется
+"aabbcde" → 2          # 'a' и 'b'
+"aabBcde" → 2          # 'a' встречается дважды, 'b' — дважды (b и B)
+"indivisibility" → 1   # 'i' встречается шесть раз
+"Indivisibilities" → 2 # 'i' — семь раз, 's' — два раза
+"aA11" → 2             # 'a' и '1'
+"ABBA" → 2             # 'A' и 'B' встречаются по два раза каждый */
+
+function duplicateCount(text){
+
+}
+
+
+
